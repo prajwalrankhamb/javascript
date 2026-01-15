5a6525896bb7cb4e44bdb68616b3d4e06dbe38b6
prajwalrankhamb <prajwalrankhamb7@gmail.com>
2026-01-15 20:53:11 +0530

    modify

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -37,7 +37,7 @@ const user = {
 
 
 
-//********************* / arrow functions *********************
+//*********************  arrow functions *********************
 
 const chai = () => {
     username: "prajwal"
