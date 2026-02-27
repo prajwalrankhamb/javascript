98c142fc25fed1b250f793248d999ac614688789
prajwalrankhamb <prajwalrankhamb7@gmail.com>
2026-02-27 12:54:34 +0530

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i += 1
+// }
+
+
+let myArray = ["spiderman", "ironman", "thor"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Superhero is ${myArray[arr]}`);
+//     arr ++
+// }
+
+
+let i = 11
+do {
+    console.log(`Value is ${i}`);
+    i++
+} while (i<=10);
