002211caa2ef4e64ba8f2be47f176e2ee70fd64c
prajwalrankhamb <prajwalrankhamb7@gmail.com>
2026-02-28 15:29:27 +0530

    map

---
+++ 05_iterations/seven.js
@@ -0,0 +1,10 @@
+
+const myNums = [1, 2, 3, 4, 5, 6, 7]
+
+// const newNums = myNums
+//         .map( (num) => num * 10)
+//         .map( (num) => num + 1)
+//         .filter( (num) => num > 30)
+// console.log(newNums);
+
+
